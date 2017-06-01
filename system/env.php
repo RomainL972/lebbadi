f734e18c73de3834876a4099ab5b5e7438cdb710
Romain Lebbadi-Breteau <romainl@protonmail.com>
2017-05-31 20:03:48 -0400

    debugging

--- system/env.php
+++ system/env.php
@@ -1,3 +1,14 @@
 <?php
 session_start();
 date_default_timezone_set('America/Martinique');
+
+require_once 'vendor/autoload.php';
+
+// Create the Transport
+$transport = (new Swift_SmtpTransport('ssl0.ovh.net', 465))
+  ->setUsername('romain@lebbadi.fr')
+  ->setPassword('JADOreovh')
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
